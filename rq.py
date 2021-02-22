ebcd9ec8db6c8ab337152614a6f8c3c93cab7f00
amazingfly <derekstich@gmail.com>
2021-02-22 22:24:34 +0000

    publish to amq

--- rq.py
+++ rq.py
@@ -4,12 +4,12 @@ def main():
     connection = pika.BlockingConnection(pika.URLParameters('amqp://derek:bazinga1@localhost:5672/'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='frame')
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='frame', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
