1338f2cddb7f14f29980142b8f5405408800fe83
amazingfly <derekstich@gmail.com>
2021-02-26 22:17:35 +0000

    index error fixed

--- rq.py
+++ rq.py
@@ -1,3 +1,9 @@
+import torchvision
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+model.eval()
+
+
+import torchvision.transforms as T
 import pika, sys, os
 import base64
 import cv2
@@ -6,6 +12,53 @@ from PIL import Image
 from io import BytesIO
 import numpy as np
 
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
+#getPrediction gets the bounding boxes and class of the FCNN predictions
+def getPrediction(img, threshold):
+    start = time.time()
+    transform = T.Compose([T.ToTensor()]) # Defing PyTorch Transform
+    img = transform(img) # Apply the transform to the image
+    pred = model([img]) # Pass the image to the model
+    predClass = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
+    predBoxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
+    predScore = list(pred[0]['scores'].detach().numpy())
+    print('predScore: ', predScore)
+    print('type: ',type(predScore))
+    print('len(): ',len(predScore))
+    noMatch = True
+    for x in predScore:
+        print('xxx: ',x)
+        print('threshold: ', threshold )
+        if x > threshold:
+            noMatch = False
+            print('predT: ',predScore.index(x))
+    predT = []
+    predBoxes = []
+    predClass = []
+    if not noMatch:
+        predT = [predScore.index(x) for x in predScore if x > threshold][-1] # Get list of index with score greater than threshold.
+        predBoxes = predBoxes[:predT+1]
+        predClass = predClass[:predT+1]
+    else:
+        print('XXX no matches found?')
+    print('!!! '+ str(start - time.time()) + ' seconds to finish!')    
+    return predBoxes, predClass
+
+
 def main():
     connection = pika.BlockingConnection(pika.URLParameters('amqp://derek:bazinga1@localhost:5672/'))
     channel = connection.channel()
@@ -13,6 +66,7 @@ def main():
     channel.queue_declare(queue='frame')
 
     def callback(ch, method, properties, body):
+        start = time.
         print('body: ',type(body))
         jBody = json.loads(body)
         for x in jBody:
@@ -20,7 +74,24 @@ def main():
         imdata = base64.b64decode(jBody['frame'])
         im = Image.open(BytesIO(imdata))
 
-        im.save('new.jpg')
+        threshold = 0.5
+        (boxes, classes) = getPrediction(im, threshold)
+        response = json.dumps({'boxes': boxes, 'classes': classes, 'fileName':jBody['fileName'], 'frameID': jBody['frameID']})
+        #jstr = json.dumps({"frame": base64.b64encode(frameJPG).decode('ascii'), 'fileName': fName, 'frameID': frameTotal})
+        #send back results to MQ
+        #
+        connection = pika.BlockingConnection(pika.URLParameters('amqp://derek:bazinga1@localhost:5672/'))
+        channel = connection.channel()
+
+        channel.queue_declare(queue='predictedResults')
+
+        channel.basic_publish(exchange='',
+                            routing_key='predictedResults',
+                            body= response)
+        
+        connection.close()
+
+        #im.save('new.jpg')
         #cv2.imwrite('./new.jpg', im)
         print(" [x] Received %r" % jBody['fileName'])
         
