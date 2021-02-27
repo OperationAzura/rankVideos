6be90cf649882a2215c914efa6e85ae73383d2c1
amazingfly <derekstich@gmail.com>
2021-02-27 04:03:28 +0000

    videoManagerClass rough draft

--- videoManagerClass.py
+++ videoManagerClass.py
@@ -2,6 +2,7 @@ import torchvision
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 model.eval()
 
+import logging
 import pika
 import torchvision.transforms as T
 import errno
@@ -31,6 +32,12 @@ COCO_INSTANCE_CATEGORY_NAMES = [
 ]
 class VideoManager(object):
     def __init__(self, path, fileName, cvData):
+        logging.basicConfig(filename='vidManager.log', encoding='utf-8', level=logging.DEBUG)
+
+        self.maxActiveFrames = 4
+
+        self.sentFrameCount = 0
+        self.frameArr = []
         self.path = path
         self.fileName = fileName
         self.cvData = cvData
@@ -45,61 +52,155 @@ class VideoManager(object):
         self.readSuccess = True #will control the video frame loops
         self.checkTorchDir()
         self.mp4Vid = cv2.VideoWriter('torch/'+self.fileName,cv2.VideoWriter_fourcc(*'mp4v'), 15, (self.frameWidth,self.frameHeight))
+        self.rectTh = 3
+        self.textTh = 3
+        self.textSize = 3
+        self.threshold = 0.5
+        logging.info('done with constructor')
 
-        #frameLoop is the main loop through the video frames
-        def frameLoop(self):
-            #loop over video framse, check for motion, then faces etc,, then write bounding box data to cvData object, extract detected areas as jpg, and create new video with bounding boxes
-            while self.readSuccess:
-                start = time.time()
-                #read video file 
-                (self.readSuccess, selfframe) = self.vid.read()
-                if not readSuccess:
-                    continue
-
-                self.frameTotal += 1
-
-
-        #getVid gets the video file based on the passed in file path
-        def getVid(self):
-            vid = cv2.VideoCapture(self.path + self.fileName)
-            retry = 0
-            while not vid.isOpened() and retry < 10:
-                retry += 1
-                print('vid not opened? retry: ', retry) 
+    #frameLoop is the main loop through the video frames
+    def frameLoop(self):
+        #loop over video framse, check for motion, then faces etc,, then write bounding box data to cvData object, extract detected areas as jpg, and create new video with bounding boxes
+        logging.info('starting frameLoop')
+        while self.readSuccess:
+            if self.sentFrameCount >= self.maxActiveFrames:
+                logging.info('max active frames hit')
+                time.sleep(10)
+                continue
+            
+            while self. sentFrameCount > self.maxActiveFrames:
+                print("maximum active frames hit! " + self.maxActiveFrames)
                 time.sleep(1)
-                vid  = cv2.VideoCapture(self.path + self.fileName)
-            return vid
+                
+            start = time.time()
+            #read video file 
+            (self.readSuccess, self.frame) = self.vid.read()
+            if not readSuccess:
+                continue
+            self.frameTotal += 1
+            self.data[self.frameTotal] = defaultdict(None)
+            self.frameData = self.data[self.frameTotal]
+            _,self.frameJPG = cv2.imencode('.jpg', self.frame)
+            self.jstr = json.dumps({"frame": base64.b64encode(self.frameJPG).decode('ascii'), 'fileName': self.fileName, 'frameID': self.frameTotal})
+            self.sendFrame(self.jstr)
+            self.frameArr.append({'frame': self.frame, 'data': self.frameData })
+            self.sentFrameCount += 1
+        self.finishVideo()
+        logging.info('done with frameLoop')
+        return self.cvData
+
+    #finishVideo will finish the video once all the frames are collacted
+    def finishVideo(self):
+        logging.info('starting finishVideo')
+        for i, frame in enumerate(self.frameArr):
+            cv2.imwrite('torch/' + self.fileName[:len(self.fileName)-4] +'/frame_'+ i+1 + '.jpg', frame['frame'])
+            self.mp4Vid.write(frame['frame'])
+        self.mp4Vid
+        self.mp4Vid.close()
+        self.vid.close()
+        logging.info('done with finishVideo')
+
+    #predictionHandler gets the prediction boxes and clases from the MQ
+    def predictionHandler(self):
+        logging.info('starting predictionHandler')
+        self.resCon = pika.BlockingConnection(pika.URLParameters('amqp://derek:bazinga1@localhost:5672/'))
+        self.resChan = self.resCon.channel()
+
+        self.resChan.queue_declare(queue='predictionResults')
+
+        def callback(ch, method, properties, body):
+            start = time.time()
+            print('body: ',type(body))
+            jBody = json.loads(body)
+            ###
+            ### TODO use frameID to add boxes and classes to the frameArr
+            ###
+            ###
+            self.frameArr[int(jBody['frameID'])-1]['boxes'] = jBody['boxes']
+            self.frameArr[int(jBody['frameID'])-1]['classes'] = jBody['classes']
+
+            boxes = jBody['boxes']
+            classes = jbody['classes']
+            for i in range(len(boxes)):
+                if self.classCounter[classes[i]] == 0:
+                    try:
+                        os.makedirs('torch/' + self.fileName[:len(self.fileName)-4] + '/' + classes[i])
+                    except OSError as e:
+                        if e.errno != errno.EEXIST:
+                            raise
+                
+                title = classes[i]+str(self.classCounter[classes[i]]) #class name + number of occurance for naming
+                self.classCounter.update([classes[i]]) #update the class name counter
+
+                (x, y) = boxes[i][0]
+                (xw, yh) = boxes[i][1]
+                ### convert type numpy.float32 to int
+                x = int(x)
+                y = int(y)
+                xw = int(xw)
+                yh = int(yh)
+
+                cv2.rectangle(self.frameArr[int(jBody['frameID'])-1]['frame'], (x, y), (xw, yh),color=(0, 255, 0), thickness=self.rectTh)
+                cv2.putText(self.frameArr[int(jBody['frameID'])-1]['frame'], title, (x, y),  cv2.FONT_HERSHEY_SIMPLEX, self.textSize, (0,255,0),thickness=self.textTh)
+                #crop predicted region of interest
+                predROI = self.frameArr[int(jBody['frameID'])-1]['frame'][ y:yh, x:xw ]
+                #save ROI image and data
+                predROIFilePath = 'torch/' + self.fileName[:len(self.fileName)-4] + '/' + classes[i] + '/' + title + '.jpg'
+                cv2.imwrite(predROIFilePath, predROI)
+                self.frameData[title] = {'x': x,'y':y, 'w':xw - x, 'h':yh - y,'imgPath': predROIFilePath}
+
+                for x in jBody:
+                    print('x: ',jBody[x])
+            #
+            # # consume 
+            #     
+            self.resChan.basic_consume(queue='predictedResults', on_message_callback=callback, auto_ack=True)
+            self.resChan.start_consuming()
+            self.sentFrameCount -= 1
+            logging.info('done with predictionHandler')
         
-        #checkTorchDir makes sure the directory exists or creates it
-        def checkTorchDir(self):
-            try:
-                os.makedirs('torch/' + fName[:len(fName)-4] + '/' )
-            except OSError as e:
-                if e.errno != errno.EEXIST:
-                    raise
-        
-        
 
+    #sendFrame sends the frame data to the MQ
+    def sendFrame(self, msgString):
+        logging.info('sending frame')
+        self.sendMQ('frame', msgString)
+        logging.info('frame sent')
 
-#getPrediction gets the bounding boxes and class of the FCNN predictions
-def getPrediction(frameRGB, threshold):
-    img = Image.fromarray(frameRGB)
-    transform = T.Compose([T.ToTensor()]) # Defing PyTorch Transform
-    img = transform(img) # Apply the transform to the image
-    pred = model([img]) # Pass the image to the model
-    predClass = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
-    predBoxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
-    predScore = list(pred[0]['scores'].detach().numpy())
-    predT = [predScore.index(x) for x in predScore if x > threshold][-1] # Get list of index with score greater than threshold.
-    predBoxes = predBoxes[:predT+1]
-    predClass = predClass[:predT+1]
-    return predBoxes, predClass
+    #sendMQ sends a message to a queue
+    def sendMQ(self, queue, message):
+        self.connection = pika.BlockingConnection(pika.URLParameters('amqp://derek:bazinga1@192.168.1.12:5672/'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=queue)
+        self.channel.basic_publish(exchange='',
+                      routing_key=queue,
+                      body=message)
+                      
+        self.connection.close()
+    
+    #getVid gets the video file based on the passed in file path
+    def getVid(self):
+        logging.info('getting video file: ' + self.fileName)
+        vid = cv2.VideoCapture(self.path + self.fileName)
+        retry = 0
+        while not vid.isOpened() and retry < 10:
+            retry += 1
+            print('vid not opened? retry: ', retry) 
+            time.sleep(1)
+            vid  = cv2.VideoCapture(self.path + self.fileName)
+        logging.info('got video file')
+        return vid
+    
+    #checkTorchDir makes sure the directory exists or creates it
+    def checkTorchDir(self):
+        try:
+            os.makedirs('torch/' + fName[:len(fName)-4] + '/' )
+        except OSError as e:
+            if e.errno != errno.EEXIST:
 
-cvDataPath = 'torchData.json'
-
+   
 #CollectCVData will read in video fiels, use motion, face, and catface detection, compare them, store positional data and collect the detected areas as jpg for later model training
 def CollectCVData():
-    
+    cvDataPath = 'torchData.json'
     paths = ['/home/derek/securityCams/cmpVids/']
     #Load existing CVData files, load them into defaultdict, and skp existing file names
     cvData = defaultdict(None)
@@ -116,130 +217,12 @@ def CollectCVData():
                 print('file ' + fName + ' already exists in rankings.  SKIPPING')
                 continue
             print('file: ', p+fName)
-            cvData = Detect(p, fName, cvData)
+            vidManager = VideoManager(p, fName, cvData)
+            cvData = vidManager.frameLoop()
             with open(cvDataPath, 'w') as outfile:
                 json.dump(cvData, outfile)
                 outfile.close()
-#Detect will check each frame for motion, faces, and cats then log there location data to a json file and store detected areas as jpg
-def Detect(p, fName, cvData):
-    
-    
-        ###
-        # ###
-        # ###
-        # ###
-        # ###
-        # ###
-        ###      
-        #set json objects for logging json data
-        data[frameTotal] = defaultdict(None)
-        frameData = data[frameTotal]
-        
-        ###new detection stuff
-        rectTh = 3
-        textTh = 3
-        textSize = 3
-        threshold = 0.5
 
-        #
-        #convert frame to bytes
-        #send fram to MQ
-        #
-
-        print('frame type: ', type(frame))
-        _,frameJPG = cv2.imencode('.jpg', frame)
-        print('frameJPG type: ', type(frameJPG))
-        jstr = json.dumps({"frame": base64.b64encode(frameJPG).decode('ascii'), 'fileName': fName, 'frameID': frameTotal})
-        print('jstr type: ', type(jstr))
-
-        connection = pika.BlockingConnection(pika.URLParameters('amqp://derek:bazinga1@192.168.1.12:5672/'))
-        channel = connection.channel()
-        channel.queue_declare(queue='frame')
-        channel.basic_publish(exchange='',
-                      routing_key='frame',
-                      body=jstr)
-                      
-        connection.close()
-
-        #
-        #Connect to response queue
-        #
-        con = pika.BlockingConnection(pika.URLParameters('amqp://derek:bazinga1@localhost:5672/'))
-        chan = con.channel()
-
-        chan.queue_declare(queue='predictionResults')
-
-        def callback(ch, method, properties, body):
-            start = time.time()
-            print('body: ',type(body))
-            jBody = json.loads(body)
-            for x in jBody:
-                print('x: ',jBody[x])
-        #
-        # # consume 1?
-        #     
-        chan.basic_consume(queue='predictedResults', on_message_callback=callback, auto_ack=True)
-
-        print(' [*] Waiting for messages. To exit press CTRL+C')
-        chan.start_consuming()
-        sys.exit(0)
-        frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        boxes, predClass = getPrediction(frameRGB, threshold) 
-        for i in range(len(boxes)):
-            if classCounter[predClass[i]] == 0:
-                try:
-                    os.makedirs('torch/' + fName[:len(fName)-4] + '/' + predClass[i])
-                except OSError as e:
-                    if e.errno != errno.EEXIST:
-                        raise
-            
-            title = predClass[i]+str(classCounter[predClass[i]]) #class name + number of occurance for naming
-            classCounter.update([predClass[i]]) #update the class name counter
-
-            (x, y) = boxes[i][0]
-            (xw, yh) = boxes[i][1]
-            ### convert type numpy.float32 to int
-            x = int(x)
-            y = int(y)
-            xw = int(xw)
-            yh = int(yh)
-
-            cv2.rectangle(frameRGB, (x, y), (xw, yh),color=(0, 255, 0), thickness=rectTh)
-            cv2.putText(frameRGB, title, (x, y),  cv2.FONT_HERSHEY_SIMPLEX, textSize, (0,255,0),thickness=textTh)
-            #crop predicted region of interest
-            predROI = frame[ y:yh, x:xw ]
-            #save ROI image and data
-            predROIFilePath = 'torch/' + fName[:len(fName)-4] + '/' + predClass[i] + '/' + title + '.jpg'
-            cv2.imwrite(predROIFilePath, predROI)
-            frameData[title] = {'x': x,'y':y, 'w':xw - x, 'h':yh - y,'imgPath': predROIFilePath}
-
-        cv2.imwrite('torch/' + fName[:len(fName)-4] +'/frame_'+ str(frameTotal) + '.jpg', frameRGB)
-        #write frame to video writter
-        mp4Vid.write(frameRGB)
-        #fmp4Vid.write(frameRGB)
-        #frameBGR = cv2.cvtColor(frameRGB, cv2.COLOR_RGB2BGR)
-        #mp4BGRVid.write(frameBGR)
-        #fmp4BGRVid.write(frameBGR)
-        print('finished frame: ' + str(frameTotal) + 'of file: ' + fName)
-        print(str(start - time.time()))
-    #End frame loop
-
-    #Release video capture object
-    vid.release() #release source video file
-      
-    mp4Vid
-    mp4Vid.release()
-    #fmp4Vid
-    #fmp4Vid.release()
-    #mp4BGRVid
-    #mp4BGRVid.release()
-    #fmp4BGRVid
-    #fmp4BGRVid.release()
-    
-    #remove empty ranking data and return rankings data
-    if len(cvData[fName]) < 1:
-        del cvData[fName]
-    return cvData
 
 if __name__ == "__main__":
     print('starting ranking')
